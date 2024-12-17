4198fbfaf51fca48565aa8dbba2f9040fa207d83
Rohit Vedulla <122700554+Rvedulla@users.noreply.github.com>
2024-12-16 19:29:57 -0800

    port back to 3000

--- server.js
+++ server.js
@@ -68,7 +68,7 @@ app.get('*', (req, res) => {
   res.sendFile(join(__dirname, 'dist', 'index.html'));
 });
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
